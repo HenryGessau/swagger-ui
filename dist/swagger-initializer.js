8deaa0f49cffa9b1dd5595dedb32d514b174b487
Henry Gessau <HenryGessau@users.noreply.github.com>
2026-02-22 23:04:36 -0500

    example for issue #10737

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,8 +3,9 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "subnet.yaml",
     dom_id: '#swagger-ui',
+    defaultModelRendering: 'model',
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
